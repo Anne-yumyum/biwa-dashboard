750f935e3b917f9830b029442d2d708b98d9f742
Anne <anne0706@icloud.com>
2026-06-15 23:42:29 +0900

    feat: アイコン改善・PNG生成

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,6 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait',
     icons: [
       {
+        src: '/icons/icon.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+        purpose: 'any',
+      },
+      {
         src: '/icons/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
